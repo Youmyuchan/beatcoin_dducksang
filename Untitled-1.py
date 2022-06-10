9a6565b0b87ae5fc908d4f7f6a855519581c1b60
염유찬 <103236570+Youmyuchan@users.noreply.github.com>
2022-06-10 14:56:26 +0900

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,24 @@
+#a =1
+#if a == 30:
+        #print("dfs")
+        #print("dtfs")
+#print("hello"*8)
+
+
+#s = "w"
+#print(type(s))
+#x = None
+#s = 1 
+#y ="agADAHSD"
+#print(x)
+#101010101011101011000101
+#a = 10
+#a+=10
+#print(a)
+#a = -10
+#b = 3
+#c = 4
+#d = 5
+#print((a+b)*(c+d))
+# = input("wfigksjikl")
+#print(a)    
